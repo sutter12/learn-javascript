3d83dace2531d274656568a644961cd58509c1be
Alex Sutter <46454849+sutter12@users.noreply.github.com>
2022-06-01 14:55:17 -0400

    platformer part 1
    end of video 1

--- platformer/drawer.js
+++ platformer/drawer.js
@@ -1,30 +1,51 @@
-// https://www.youtube.com/watch?v=oMjrl_Txps8&ab_channel=MichaelHulbert
 function rectangle(x, y, w, h) {
     render.beginPath();
     render.rect(x, y, w, h);
     render.fill();
     render.stroke();
-  }//end rectangle
-  
-  function fillColor(r, g, b) {
+}//end rectangle
+
+function fillColor(r, g, b) {
     r = String(r);
     g = String(g);
     b = String(b);
     render.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-  }//end fillColor
-  
-  function strokeSize(size) {
+}//end fillColor
+
+function noFill() {
+    render.fillStyle = "rgba(0, 0, 0, 0)";
+}//end noFill
+
+function strokeSize(size) {
     render.lineWidth = String(size);
-  }//end strokeSize
-  
-  function background(r, g, b) {
-    fillColor(r, g, b);
-    rectangle(0, 0, 0);
-  }//end background
-  
-  function strokeColor(r, g, b) {
+}//end strokeSize
+
+function strokeColor(r, g, b) {
     r = String(r);
     g = String(g);
     b = String(b);
     render.strokeStyle = "rgb(" + r + "," + g + "," + b + ")";
-  }//end strokeColor
+}//end strokeColor
+
+function noStroke() {
+    render.strokeStyle = "rgba(0, 0, 0, 0)";
+}//end noStroke
+
+function background(r, g, b) {
+    fillColor(r, g, b);
+    rectangle(0, 0, width, height);
+}//end background
+
+function line(x1, y1, x2, y2) {
+    render.beginPath();
+    render.moveTo(x1, y1);
+    render.lineTo(x2, y2);
+    render.stroke();
+}//end line
+
+function circle(x, y, r) {
+    render.beginPath();
+    render.arc(x, y, r, 0, 2*Math.PI);
+    render.fill();
+    render.stroke();
+}//end circle
--- platformer/main.js
+++ platformer/main.js
@@ -1,7 +1,40 @@
+// https://www.youtube.com/watch?v=oMjrl_Txps8&ab_channel=MichaelHulbert
+
 var canvas = document.getElementById("gamescreen");
 var render = canvas.getContext("2d");
 
-fillColor(255, 0, 255);
-strokeColor(0, 255, 0);
-strokeSize(4);
-rectangle(25, 25, 50, 100);
+//used for size of screen
+var width; 
+var height;
+
+//fix white bar on top and left
+canvas.style.left = "0px";
+canvas.style.top = "0px";
+canvas.style.position = "absolute";
+
+//redraws everything anytime window is resized or created
+window.onresize = function() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    canvas.width = width;
+    canvas.height = height;
+    
+    background(25, 25, 25);
+
+    fillColor(255, 0, 255);
+    // noFill()
+    strokeColor(0, 255, 0);
+    strokeSize(4);
+    rectangle(25, 25, 50, 100);
+
+    //line does not need a fill
+    strokeColor(255, 0, 0);
+    line(50, 100, 25, 300);
+
+    //circle
+    noStroke();
+    fillColor(0, 0, 255);
+    circle(200, 200, 75)
+}
+
+window.onresize();
